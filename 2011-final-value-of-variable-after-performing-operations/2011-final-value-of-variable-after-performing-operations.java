3b5f3b7329a1c686f5ea561fc56eb1af83e92497
Kiran <119686838+kiran5harma@users.noreply.github.com>
2023-09-28 22:50:11 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (7.32%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
@@ -1,17 +1,27 @@
 class Solution {
     public int finalValueAfterOperations(String[] operations) {
         
-        HashMap<String, Integer> map = new HashMap<String, Integer>();
-        map.put("--X",-1);
-        map.put("X--",-1);
-        map.put("++X",1);
-        map.put("X++",1);
+        //Using HASHMAP---1ms solution
+//         HashMap<String, Integer> map = new HashMap<String, Integer>();
+//         map.put("--X",-1);
+//         map.put("X--",-1);
+//         map.put("++X",1);
+//         map.put("X++",1);
         
+//         int ans=0;
+        
+//         for(String op:operations){
+//             ans+=map.get(op);
+//         }
+//         return ans;
+        
+        // 0ms
         int ans=0;
-        
-        for(String op:operations){
-            ans+=map.get(op);
+        for( String op: operations){
+            ans+= 44 - op.charAt(1);
         }
+        
         return ans;
+        
     }
 }
