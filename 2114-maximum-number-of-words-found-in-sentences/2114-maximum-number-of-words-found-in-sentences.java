f037f7f6448543ba2a5ef10c2180eb91278ee60d
Kiran <119686838+kiran5harma@users.noreply.github.com>
2023-09-29 19:27:22 +0530

    Time: 3 ms (89.90%), Space: 43 MB (44.30%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
@@ -2,12 +2,15 @@ class Solution {
     public int mostWordsFound(String[] sen) {
         int count=1;
         int max=0;
+        
         for(int i=0;i<sen.length;i++){
             count=1;
-            for (int j=0;j<sen[i].length();j++){
-                if(sen[i].charAt(j)==' ')
-                    count++;
-            }
+            String arr[] = sen[i].split(" ");
+            // for (int j=0;j<sen[i].length();j++){
+            //     if(sen[i].charAt(j)==' ')
+            //         count++;
+            // }
+            count= arr.length;
             if(max<count)
                 max=count;
         }
