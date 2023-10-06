41d7b6b23637cdd1adc54026cfa8178e99a4bba6
Kiran <119686838+kiran5harma@users.noreply.github.com>
2023-10-06 07:19:40 +0530

    Time: 12 ms (48.32%), Space: 45 MB (35.26%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -23,48 +23,48 @@ class Solution {
         
         //Optimal...12MS
         
-//         Arrays.sort(nums);
-//         int m1 = nums[0]*nums[1]*nums[n-1];
-//         int m2 = nums[n-1]*nums[n-2]*nums[n-3];
+        Arrays.sort(nums);
+        int m1 = nums[0]*nums[1]*nums[n-1];
+        int m2 = nums[n-1]*nums[n-2]*nums[n-3];
         
-//         if(m1>m2)
-//             return m1;
-//         else 
-//             return m2;
+        if(m1>m2)
+            return m1;
+        else 
+            return m2;
         
-        //Again..how deep is your lovee
+        //Again..how deep is your lovee............15 MS -_- 
         
-        int max[] = {Integer.MIN_VALUE,Integer.MIN_VALUE,Integer.MIN_VALUE};
-        int min[] = {Integer.MAX_VALUE,Integer.MAX_VALUE};
+//         int max[] = {Integer.MIN_VALUE,Integer.MIN_VALUE,Integer.MIN_VALUE};
+//         int min[] = {Integer.MAX_VALUE,Integer.MAX_VALUE};
         
-        for(int i=0;i<n;i++){
-            if(nums[i]>max[0]){
-                max[2]= max[1];
-                max[1]= max[0];
-                max[0]= nums[i];
-            }
-            else if(nums[i]>max[1]){
-                max[2]= max[1];
-                max[1]= nums[i];
-            }
-            else if(nums[i]>max[2])
-                max[2]= nums[i];
+//         for(int i=0;i<n;i++){
+//             if(nums[i]>max[0]){
+//                 max[2]= max[1];
+//                 max[1]= max[0];
+//                 max[0]= nums[i];
+//             }
+//             else if(nums[i]>max[1]){
+//                 max[2]= max[1];
+//                 max[1]= nums[i];
+//             }
+//             else if(nums[i]>max[2])
+//                 max[2]= nums[i];
             
-            if(nums[i]<min[0]){
-                min[1]= min[0];
-                min[0]= nums[i];
-            }
-            else if(nums[i]<min[1])
-                min[1]= nums[i];
-        }
+//             if(nums[i]<min[0]){
+//                 min[1]= min[0];
+//                 min[0]= nums[i];
+//             }
+//             else if(nums[i]<min[1])
+//                 min[1]= nums[i];
+//         }
         
-        System.out.println(max[0]+" "+max[1]+" "+max[2]);
-        System.out.println(min[0]+" "+min[1]);
+//         System.out.println(max[0]+" "+max[1]+" "+max[2]);
+//         System.out.println(min[0]+" "+min[1]);
         
-        int m1 = max[0]*max[1]*max[2];
-        int m2 = min[0]*min[1]*max[0];
+//         int m1 = max[0]*max[1]*max[2];
+//         int m2 = min[0]*min[1]*max[0];
         
-        return m1>m2?m1:m2;
+//         return m1>m2?m1:m2;
         
     }
 }
