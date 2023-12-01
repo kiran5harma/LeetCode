2a09c1b8d5ab435f7ff59761c9fcfe6579100c73
Kiran <119686838+kiran5harma@users.noreply.github.com>
2023-12-01 10:50:28 +0530

    Time: 43 ms (37.45%), Space: 16.4 MB (28.02%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.py
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.py
@@ -10,12 +10,15 @@ class Solution:
         # w1 = ''.join(map(str,word1))
         # w2 = ''.join(map(str,word2))
         
-        w1 = ''.join(word1)
-        w2 = ''.join(word2)
+        # w1 = ''.join(word1)
+        # w2 = ''.join(word2)
         
         # print(w1,w2)
         # if (w1==w2):
         #     return True
         # else:
         #     return False
-        return w1==w2
+        
+        # return w1==w2
+        
+        return ''.join(word1) == ''.join(word2)
