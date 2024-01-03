a1247fd8ba4ee101ec43fa13323142e6cd840f37
Kiran <119686838+kiran5harma@users.noreply.github.com>
2024-01-03 13:15:17 +0530

    Time: 76 ms (6.04%), Space: 17.5 MB (9.99%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,16 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        d={'I':[1,1],'V':[2,5],'X':[3,10],'L':[4,50],'C':[5,100],'D':[6,500],'M':[7,1000]}
+        prev = d.get(s[0])
+        ans = 0
+        for i in range(1,len(s)):
+            print(ans)
+            temp = d.get(s[i])
+            if prev[0] < temp[0]:
+                ans+= -prev[1]
+            else:
+                ans+= prev[1]
+            prev = temp
+        ans += prev[1]
+        return ans
+        
